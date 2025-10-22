f851627ca43a06c6e2c802fd582984866f6c430a
fxck <debianlinux432@gmail.com>
2025-10-22 19:43:50 -0300

    Add files via upload

---
+++ boot.asm
@@ -0,0 +1,31 @@
+[ORG 0x7C00]
+BITS 16
+ 
+start:
+    XOR AX, AX
+    MOV DS, AX
+    MOV ES, AX
+    MOV SS, AX
+    MOV SP, 0x7C00
+ 
+    MOV SI, fxck
+ 
+print_loop:
+    LODSB
+    OR AL, AL
+    JZ gozei
+ 
+    MOV AH, 0x0E
+    INT 0x10
+ 
+    JMP print_loop
+ 
+gozei:
+    CLI
+    HLT
+    JMP gozei
+ 
+fxck: DB "eu amo o artemis :3", 0x0D, 0x0A, 0
+ 
+times 510 - ($ - $$) DB 0
+DW 0xAA55
